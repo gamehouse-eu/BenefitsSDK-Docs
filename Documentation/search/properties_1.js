0215e786407c543d3d00f1f6c200e32852db4d01
gamehouse-ct <gamehouse-ct@gamehouse.com>
2024-10-22 12:33:16 +0000

    Documentation for release 1.0.0-rc10

--- Documentation/search/properties_1.js
+++ Documentation/search/properties_1.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['instance_72',['instance',['../dd/d06/interfaceGameHouse_1_1BenefitsSDK_1_1IBenefitsSdk.html#a692f46457e222ed9cee657f19c82885a',1,'GameHouse::BenefitsSDK::IBenefitsSdk']]],
-  ['isauthenticated_73',['IsAuthenticated',['../dd/d06/interfaceGameHouse_1_1BenefitsSDK_1_1IBenefitsSdk.html#a71ea52088e9657929a896ade1f12ec12',1,'GameHouse::BenefitsSDK::IBenefitsSdk']]],
-  ['issdkinitialized_74',['IsSdkInitialized',['../dd/d06/interfaceGameHouse_1_1BenefitsSDK_1_1IBenefitsSdk.html#a2769ab2ad37ca20cf7c2fb389465d7c6',1,'GameHouse::BenefitsSDK::IBenefitsSdk']]],
+  ['instance_72',['Instance',['../dd/d06/interfaceGameHouse_1_1BenefitsSDK_1_1IBenefitsSdk.html#aa381b6e1d1b0378973fee88d1e7cee1b',1,'GameHouse::BenefitsSDK::IBenefitsSdk']]],
+  ['issdkinitialized_73',['IsSdkInitialized',['../dd/d06/interfaceGameHouse_1_1BenefitsSDK_1_1IBenefitsSdk.html#a2769ab2ad37ca20cf7c2fb389465d7c6',1,'GameHouse::BenefitsSDK::IBenefitsSdk']]],
+  ['isuserloggedin_74',['IsUserLoggedIn',['../dd/d06/interfaceGameHouse_1_1BenefitsSDK_1_1IBenefitsSdk.html#a669e5da9f643dafe92261a08acd844ce',1,'GameHouse::BenefitsSDK::IBenefitsSdk']]],
   ['isusersubscribed_75',['IsUserSubscribed',['../dd/d06/interfaceGameHouse_1_1BenefitsSDK_1_1IBenefitsSdk.html#abaaa51293b2ab8f4f90d8c91dcfa8bb3',1,'GameHouse::BenefitsSDK::IBenefitsSdk']]]
 ];
