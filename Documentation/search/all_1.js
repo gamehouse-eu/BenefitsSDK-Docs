5ca15e16dde0fe5863dc11546185e997402dc537
gamehouse-ct <gamehouse-ct@gamehouse.com>
2025-08-11 10:38:35 +0000

    Documentation for release 1.0.3

--- Documentation/search/all_1.js
+++ Documentation/search/all_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['clientid_0',['ClientId',['../da/df0/classGameHouse_1_1BenefitsSDK_1_1BenefitsSDK_1_1Internal_1_1Backend_1_1ClientId.html',1,'GameHouse::BenefitsSDK::BenefitsSDK::Internal::Backend']]]
+  ['clientid_0',['ClientId',['../d6/d49/classGameHouse_1_1BenefitsSDK_1_1Internal_1_1Backend_1_1ClientId.html',1,'GameHouse::BenefitsSDK::Internal::Backend']]]
 ];
--- Documentation/search/namespaces_0.js
+++ Documentation/search/namespaces_0.js
@@ -2,16 +2,13 @@ var searchData=
 [
   ['gamehouse_0',['GameHouse',['../dd/d07/namespaceGameHouse.html',1,'']]],
   ['gamehouse_3a_3abenefitssdk_1',['BenefitsSDK',['../da/dd8/namespaceGameHouse_1_1BenefitsSDK.html',1,'GameHouse']]],
-  ['gamehouse_3a_3abenefitssdk_3a_3abackend_2',['Backend',['../df/d51/namespaceGameHouse_1_1BenefitsSDK_1_1Backend.html',1,'GameHouse::BenefitsSDK']]],
-  ['gamehouse_3a_3abenefitssdk_3a_3abenefitssdk_3',['BenefitsSDK',['../da/d3d/namespaceGameHouse_1_1BenefitsSDK_1_1BenefitsSDK.html',1,'GameHouse::BenefitsSDK']]],
-  ['gamehouse_3a_3abenefitssdk_3a_3abenefitssdk_3a_3ainternal_4',['Internal',['../d2/d3d/namespaceGameHouse_1_1BenefitsSDK_1_1BenefitsSDK_1_1Internal.html',1,'GameHouse::BenefitsSDK::BenefitsSDK']]],
-  ['gamehouse_3a_3abenefitssdk_3a_3abenefitssdk_3a_3ainternal_3a_3abackend_5',['Backend',['../d8/d8d/namespaceGameHouse_1_1BenefitsSDK_1_1BenefitsSDK_1_1Internal_1_1Backend.html',1,'GameHouse::BenefitsSDK::BenefitsSDK::Internal']]],
-  ['gamehouse_3a_3abenefitssdk_3a_3aeventtracking_6',['EventTracking',['../df/dde/namespaceGameHouse_1_1BenefitsSDK_1_1EventTracking.html',1,'GameHouse::BenefitsSDK']]],
-  ['gamehouse_3a_3abenefitssdk_3a_3ainternal_7',['Internal',['../df/dd3/namespaceGameHouse_1_1BenefitsSDK_1_1Internal.html',1,'GameHouse::BenefitsSDK']]],
-  ['gamehouse_3a_3abenefitssdk_3a_3ainternal_3a_3aappinfo_8',['AppInfo',['../d2/d5a/namespaceGameHouse_1_1BenefitsSDK_1_1Internal_1_1AppInfo.html',1,'GameHouse::BenefitsSDK::Internal']]],
-  ['gamehouse_3a_3abenefitssdk_3a_3ainternal_3a_3abackend_9',['Backend',['../d6/dec/namespaceGameHouse_1_1BenefitsSDK_1_1Internal_1_1Backend.html',1,'GameHouse::BenefitsSDK::Internal']]],
-  ['gamehouse_3a_3abenefitssdk_3a_3ainternal_3a_3aeventtracking_10',['EventTracking',['../d1/d3a/namespaceGameHouse_1_1BenefitsSDK_1_1Internal_1_1EventTracking.html',1,'GameHouse::BenefitsSDK::Internal']]],
-  ['gamehouse_3a_3abenefitssdk_3a_3ainternal_3a_3astorage_11',['Storage',['../d9/dda/namespaceGameHouse_1_1BenefitsSDK_1_1Internal_1_1Storage.html',1,'GameHouse::BenefitsSDK::Internal']]],
-  ['gamehouse_3a_3abenefitssdk_3a_3asettings_12',['Settings',['../d2/dee/namespaceGameHouse_1_1BenefitsSDK_1_1Settings.html',1,'GameHouse::BenefitsSDK']]],
-  ['gamehouse_3a_3abenefitssdk_3a_3astorage_13',['Storage',['../dd/db5/namespaceGameHouse_1_1BenefitsSDK_1_1Storage.html',1,'GameHouse::BenefitsSDK']]]
+  ['gamehouse_3a_3abenefitssdk_3a_3aeventtracking_2',['EventTracking',['../df/dde/namespaceGameHouse_1_1BenefitsSDK_1_1EventTracking.html',1,'GameHouse::BenefitsSDK']]],
+  ['gamehouse_3a_3abenefitssdk_3a_3ainternal_3',['Internal',['../df/dd3/namespaceGameHouse_1_1BenefitsSDK_1_1Internal.html',1,'GameHouse::BenefitsSDK']]],
+  ['gamehouse_3a_3abenefitssdk_3a_3ainternal_3a_3aappinfo_4',['AppInfo',['../d2/d5a/namespaceGameHouse_1_1BenefitsSDK_1_1Internal_1_1AppInfo.html',1,'GameHouse::BenefitsSDK::Internal']]],
+  ['gamehouse_3a_3abenefitssdk_3a_3ainternal_3a_3abackend_5',['Backend',['../d6/dec/namespaceGameHouse_1_1BenefitsSDK_1_1Internal_1_1Backend.html',1,'GameHouse::BenefitsSDK::Internal']]],
+  ['gamehouse_3a_3abenefitssdk_3a_3ainternal_3a_3aeventtracking_6',['EventTracking',['../d1/d3a/namespaceGameHouse_1_1BenefitsSDK_1_1Internal_1_1EventTracking.html',1,'GameHouse::BenefitsSDK::Internal']]],
+  ['gamehouse_3a_3abenefitssdk_3a_3ainternal_3a_3aminijson_7',['MiniJson',['../da/da7/namespaceGameHouse_1_1BenefitsSDK_1_1Internal_1_1MiniJson.html',1,'GameHouse::BenefitsSDK::Internal']]],
+  ['gamehouse_3a_3abenefitssdk_3a_3ainternal_3a_3astorage_8',['Storage',['../d9/dda/namespaceGameHouse_1_1BenefitsSDK_1_1Internal_1_1Storage.html',1,'GameHouse::BenefitsSDK::Internal']]],
+  ['gamehouse_3a_3abenefitssdk_3a_3asettings_9',['Settings',['../d2/dee/namespaceGameHouse_1_1BenefitsSDK_1_1Settings.html',1,'GameHouse::BenefitsSDK']]],
+  ['gamehouse_3a_3abenefitssdk_3a_3astorage_10',['Storage',['../dd/db5/namespaceGameHouse_1_1BenefitsSDK_1_1Storage.html',1,'GameHouse::BenefitsSDK']]]
 ];
